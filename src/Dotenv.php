8fe113e73b665dc90f3f74f92307b41f6bc66364
Francesco Bianco <info.francescobianco@gmail.com>
2017-11-23 18:42:52 +0100

    Version 0.0.2

--- src/Dotenv.php
+++ src/Dotenv.php
@@ -13,13 +13,53 @@
 
 namespace Javanile\Dotenv;
 
-class Dotenv
+class Dotenv extends
 {
     /**
-     * Constructor.
+     * Gets the value of an environment variable.
+     *
+     * @param  string  $key
+     * @param  mixed   $default
+     * @return mixed
      */
-    public function __construct()
+    public function get($key, $default = null)
     {
+        $value = getenv($key);
 
+        if ($value === false) {
+            return $this->value($default);
+        }
+
+        switch (strtolower($value)) {
+            case 'true':
+            case '(true)':
+                return true;
+            case 'false':
+            case '(false)':
+                return false;
+            case 'empty':
+            case '(empty)':
+                return '';
+            case 'null':
+            case '(null)':
+                return;
+        }
+
+        if (strlen($value) > 1 && $value[0] == '"' && $value[strlen($value) - 1] == '"') {
+            return substr($value, 1, -1);
+        }
+
+        return $value;
+    }
+
+    /**
+     * Return the default value of the given value.
+     *
+     * @param  mixed  $value
+     * @return mixed
+     */
+    private function value($value)
+    {
+        return $value instanceof Closure ? $value() : $value;
     }
 }
